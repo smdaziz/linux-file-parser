234cb387f98269de48ec7b756dcc944f27a17201
Mohammed Abdul Aziz Syed <86378050+msyed-godaddy@users.noreply.github.com>
2021-08-12 18:00:13 -0500

    Update file_reader.sh

--- file_reader.sh
+++ file_reader.sh
@@ -4,6 +4,9 @@ if [ $# -eq 0 ]
     echo "Please pass delimited file with serverIP and path"
     exit 1
 fi
-while IFS=',' read -r -a array; do
-    perl perl_script.pl ${array[0]} ${array[1]}
+#while IFS=',' read -r -a array; do
+#    perl perl_script.pl ${array[0]} ${array[1]}
+#done < "$1"
+while read -r -a path; do
+    perl perl_script.pl path
 done < "$1"
