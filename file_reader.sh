547e0c27ea832b55362d24260f72bea87e528c0c
Mohammed Abdul Aziz Syed <smdaziz@gmail.com>
2021-08-15 22:25:29 -0500

    Update file_reader.sh

--- file_reader.sh
+++ file_reader.sh
@@ -6,8 +6,9 @@ if [ $# -eq 0 ]
 fi
 while IFS=',' read -r -a array; do
 #    perl perl_script.pl ${array[0]} ${array[1]}
+     perl perl_script.pl ${array[0]} ${array[1]} ${array[2]}
 #    pbrun -h ${array[0]} /perl_path/perl_script.pl ${array[1]} ${array[2]}
-    pbrun -h ${array[0]} /perl_path/perl_script.pl ${array[1]} /tmp/${array[0]}.txt ## >> appends output towards end of file /tmp/${array[0]}.txt
+#    pbrun -h ${array[0]} /perl_path/perl_script.pl ${array[1]} /tmp/${array[0]}.txt ## >> appends output towards end of file /tmp/${array[0]}.txt
 done < "$1"
 #while read -r path; do
 #    perl perl_script.pl $path
