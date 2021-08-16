31235da7f1af7a5b2c3c2246049a5b400dc6cea3
Mohammed Abdul Aziz Syed <smdaziz@gmail.com>
2021-08-15 22:24:03 -0500

    Update perl_script.pl

--- perl_script.pl
+++ perl_script.pl
@@ -1,9 +1,15 @@
 #!/usr/bin/perl
 use warnings;
-if (@ARGV == 0 && -t STDIN && -t STDERR) { 
+if (@ARGV == 0 && -t STDIN && -t STDERR) {
     print("Please pass serverIP and path as arguments to this script\n");
 }
-my ($serverIP, $path) = @ARGV;
+my ($serverIP, $path, $output) = @ARGV;
 if (defined $serverIP & defined $path) {
     print("Server IP $serverIP | Path : $path\n");
+#    open(FH, '>', $output) or die $!; ## opening file with '>' doesn't append to the file, rather overrides previously written content
+    open(FH, '>>', $output) or die $!; ## opening file with '>>' appends content towards the end of file
+    print FH "$serverIP \n"; ## print/write content to the file
+    close(FH); ## close the file
+} elsif(defined $serverIP) {
+    print("Server Path $serverIP\n");
 }
