4d4dcb7bf9f3dfc5607c2983a7c7d1a824c10789
Mohammed Abdul Aziz Syed <smdaziz@gmail.com>
2021-08-16 23:10:54 -0500

    Update perl_script.pl

--- perl_script.pl
+++ perl_script.pl
@@ -7,9 +7,9 @@ my ($serverIP, $path, $output) = @ARGV;
 if (defined $serverIP & defined $path) {
     print("Server IP $serverIP | Path : $path\n");
 #    open(FH, '>', $output) or die $!; ## opening file with '>' doesn't append to the file, rather overrides previously written content
-    open(FH, '>>', $output) or die $!; ## opening file with '>>' appends content towards the end of file
-    print FH "$serverIP \n"; ## print/write content to the file
-    close(FH); ## close the file
+#    open(FH, '>>', $output) or die $!; ## opening file with '>>' appends content towards the end of file
+#    print FH "$serverIP \n"; ## print/write content to the file
+#    close(FH); ## close the file
 } elsif(defined $serverIP) {
     print("Server Path $serverIP\n");
 }
